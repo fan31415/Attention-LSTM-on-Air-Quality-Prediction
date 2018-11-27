f0eebb23cc46effd5ab1fba83523a829d76b644c
fan31415 <fan12356789@gmail.com>
2018-11-27 17:33:24 +0800

    test2

--- model.py
+++ model.py
@@ -153,15 +153,15 @@ class LSTM_model(object):
             cell = tf.contrib.cudnn_rnn.CudnnLSTM(2, state_size, kernel_initializer = UniformInitializer,
                                                         bias_initializer = UniformInitializer)
 
-            self.init_state = []
-
-            self.init_state.append(tf.get_variable('hidden_h', tf.random_uniform([2, batch_size, state_size])))
-            self.init_state.append(tf.get_variable('hidden_c', tf.random_uniform([2, batch_size, state_size])))
+            self.init_state = [None] * 2
+            self.init_state[0] = tf.get_variable('hidden_h', [2, batch_size, state_size], initializer=UniformInitializer)
+            self.init_state[1] = tf.get_variable('hidden_c', [2, batch_size, state_size], initializer=UniformInitializer)
             # self.init_state = tf.get_variable('initial_state',
             #                                   [tf.random_uniform(cell.state_shape(batch_size)[0]),
             #                                    tf.random_uniform(cell.state_shape(batch_size)[1])])
             state = self.init_state
-            outputs, state[0], state[1] = cell(self.inputs, input_h=state[0], input_c=state[1])
+            
+            outputs, state[0], state[1] = cell(self.inputs, input_h= state[0], input_c=state[1])
         else:
             stacked_cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.LSTMCell(state_size) \
                                                         for _ in range(layer_num)])
